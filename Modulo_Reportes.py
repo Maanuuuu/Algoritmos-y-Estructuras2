1a41faf1b27567dd9568ffe1bbd21f703cc158b8
Farrauto <jvfarrauto06@gmail.com>
2024-06-19 04:13:42 -0400

    metodo del modulo reporte consult_tarea_est LISTO

--- Modulo_Reportes.py
+++ Modulo_Reportes.py
@@ -1,4 +1,5 @@
 import Gestion_Proyectos as gp
+import datos_abstracto as da
 
 class Reporte:
     def __init__(self):
@@ -8,11 +9,14 @@ class Reporte:
 
     def menu(self,proyectitos):
         while True:
-            print("1. Consultar tarea por estado: ")
-            print("2. Filtrado por fechas: ")
-            print("3. Filtrado de proyectos:")
-            print("4. Listar subtareas: ")
-            print("5. Salir")
+            print("-------------------------------")
+            print("Menu del modulo de reportes")
+            print("Elija la operacion a realizar:")
+            print("1.- Consultar tarea por estado.")
+            print("2.- Filtrado por fechas.")
+            print("3.- Filtrado de proyectos.")
+            print("4.- Listar subtareas.")
+            print("5.- Salir")
             opcion = int(input("Ingrese la opcion: "))
             if opcion == 1:
                 self.consult_tarea_est(self.proyectitos)
@@ -28,18 +32,27 @@ class Reporte:
                 print("Opcion invalida")
 
     def consult_tarea_est(self,proyectitos):
-        est=input("Indica el estado de la tarea: ")
         list_estados=[]
+        list_pilas=[]
+        cont=0
+        dicc_estados={}
         for proyecto in self.proyectitos:
             for tarea in proyecto.tareas:
                 if tarea.estado in list_estados:
-                    pass
+                    pos=dicc_estados[tarea.estado]
+                    list_pilas[pos].agregar_tarea(tarea)
+
                 else:
                     list_estados.append(tarea.estado)
-                    
-                    
+                    list_pilas.append(da.Pila_Tareas())
+                    list_pilas[cont].agregar_tarea(tarea)
+                    dicc_estados[tarea.estado]=cont
+                    cont+=1
                 
-        
+        for i in range(len(list_pilas)):
+            print("-------------------------------------------------")
+            print("Tareas con estado: ",list_estados[i])
+            print(list_pilas[i].mostrar_pila_tareas())
 
     def filtrado_fechas(self,proyectitos):
         pass
